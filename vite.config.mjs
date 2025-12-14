4995e3b759dfde98bd9231ff2f47b39b6af61be7
sa.lebedev <sa.lebedev@tensor.ru>
2025-12-14 12:49:50 +0300

    fix: no 13 macos

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,13 @@
-// vite.config.mjs
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+    base: '/ci-cd-lab-2/',
     plugins: [react()],
     test: {
         environment: 'jsdom',
         include: ['tests/**/*.test.{js,jsx}'],
         setupFiles: './tests/setupTests.js',
-        coverage: {
-            provider: 'v8'
-        }
+        coverage: { provider: 'v8' }
     }
 });
